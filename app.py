46a731f3e91c948427ed6dda2fd08a0eff821ea8
phaninder2424 <57430690+phaninder2424@users.noreply.github.com>
2022-02-27 02:06:14 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from ast import increment_lineno
 import streamlit as st
 import pandas as pd 
 import numpy as np
-import matplotlib.pyplot as plt
 from textblob import TextBlob
 import seaborn as sns
 import textblob
